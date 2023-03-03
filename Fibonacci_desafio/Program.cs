793f05763cb879282a601b8b5d1463aa6f3c758c
maikeliani <maikel.iani31@gmail.com>
2023-03-03 14:50:34 -0300

    Add project files.

---
+++ Fibonacci_desafio/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double anterior = 0, posicao, atual = 1;
+        Console.WriteLine(" Informe ate qual posição da sequencia deseja ver: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        do
+        {
+            Console.Write(atual + " ");
+            atual = atual + anterior;
+            anterior = atual - anterior;
+        
+        } while (atual <= posicao);
+
+    }
+}
